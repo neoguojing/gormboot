a801b63a2b58d4c3f8a594679582ea810c6a5c55
guoerjun <guoerjun@sensetime.com>
2023-03-03 16:45:41 +0800

    up

---
+++ mysql.go
@@ -0,0 +1,90 @@
+package gormboot
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db     *gorm.DB
+	models = []interface{}{}
+)
+
+func RegisterModel(model interface{}) {
+	models = append(models, model)
+}
+
+func init() {
+	source := os.Getenv("db.source")
+	dbPath := os.Getenv("db.path")
+	maxIdle := os.Getenv("db.maxIdle")
+	maxConn := os.Getenv("db.maxConn")
+	enableLog := os.Getenv("db.log")
+
+	if source == "" {
+		log.Fatal("env db.source was empty")
+	}
+
+	if dbPath == "" {
+		log.Fatal("env db.path was empty")
+	}
+
+	var err error
+	db, err = gorm.Open(source, dbPath)
+	if err != nil {
+		panic(err)
+	}
+
+	if enableLog != "" {
+		db.LogMode(true)
+	}
+
+	if maxConn != "" {
+		maxC, err := strconv.Atoi(maxConn)
+		if err == nil {
+			db.DB().SetMaxOpenConns(maxC)
+		} else {
+			log.Print(err.Error())
+		}
+
+	}
+
+	if maxIdle != "" {
+		maxI, err := strconv.Atoi(maxIdle)
+		if err == nil {
+			db.DB().SetMaxIdleConns(maxI)
+		} else {
+			log.Print(err.Error())
+		}
+
+	}
+
+	if len(models) != 0 {
+		err = db.AutoMigrate(models...).Error
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+}
+
+func getDB() *gorm.DB {
+	err := db.DB().Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func GetDB() *gorm.DB {
+	return getDB()
+}
+
+func Destroy() {
+	db.Close()
+}
