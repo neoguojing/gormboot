19afd83f72186a5fb52e85d0aee2fcf6835f34d8
guoerjun <guoerjun@sensetime.com>
2023-03-03 17:22:49 +0800

    up

--- mysql.go
+++ mysql.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"sync"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -12,6 +13,7 @@ import (
 var (
 	db     *gorm.DB
 	models = []interface{}{}
+	once   sync.Once
 )
 
 func RegisterModel(model interface{}) {
@@ -19,57 +21,62 @@ func RegisterModel(model interface{}) {
 }
 
 func init() {
-	source := os.Getenv("db.source")
-	dbPath := os.Getenv("db.path")
-	maxIdle := os.Getenv("db.maxIdle")
-	maxConn := os.Getenv("db.maxConn")
-	enableLog := os.Getenv("db.log")
 
-	if source == "" {
-		log.Fatal("env db.source was empty")
-	}
+}
 
-	if dbPath == "" {
-		log.Fatal("env db.path was empty")
-	}
+func Init() {
+	once.Do(func() {
+		source := os.Getenv("db.source")
+		dbPath := os.Getenv("db.path")
+		maxIdle := os.Getenv("db.maxIdle")
+		maxConn := os.Getenv("db.maxConn")
+		enableLog := os.Getenv("db.log")
 
-	var err error
-	db, err = gorm.Open(source, dbPath)
-	if err != nil {
-		panic(err)
-	}
-
-	if enableLog != "" {
-		db.LogMode(true)
-	}
-
-	if maxConn != "" {
-		maxC, err := strconv.Atoi(maxConn)
-		if err == nil {
-			db.DB().SetMaxOpenConns(maxC)
-		} else {
-			log.Print(err.Error())
+		if source == "" {
+			log.Fatal("env db.source was empty")
 		}
 
-	}
-
-	if maxIdle != "" {
-		maxI, err := strconv.Atoi(maxIdle)
-		if err == nil {
-			db.DB().SetMaxIdleConns(maxI)
-		} else {
-			log.Print(err.Error())
+		if dbPath == "" {
+			log.Fatal("env db.path was empty")
 		}
 
-	}
-
-	if len(models) != 0 {
-		err = db.AutoMigrate(models...).Error
+		var err error
+		db, err = gorm.Open(source, dbPath)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
-	}
 
+		if enableLog != "" {
+			db.LogMode(true)
+		}
+
+		if maxConn != "" {
+			maxC, err := strconv.Atoi(maxConn)
+			if err == nil {
+				db.DB().SetMaxOpenConns(maxC)
+			} else {
+				log.Print(err.Error())
+			}
+
+		}
+
+		if maxIdle != "" {
+			maxI, err := strconv.Atoi(maxIdle)
+			if err == nil {
+				db.DB().SetMaxIdleConns(maxI)
+			} else {
+				log.Print(err.Error())
+			}
+
+		}
+
+		if len(models) != 0 {
+			err = db.AutoMigrate(models...).Error
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	})
 }
 
 func getDB() *gorm.DB {
